e5fbc1ed464b7c4d8cf6cc71599e9d0a8031437f
Tania Papazafeiropoulou <taniapapazaf@gmail.com>
2020-12-25 22:41:35 +0200

    update node version

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const cors = require('cors')
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 // Constants
 const PORT = process.env.PORT || 8080;
 
-let requestExt = require('request-extensible');
-let RequestHttpCache = require('request-http-cache');
+let requestExt = require("request-extensible");
+let RequestHttpCache = require("request-http-cache");
 
 let httpRequestCache = new RequestHttpCache({
-  max: 10*1024*1024, // Maximum cache size (1mb) defaults to 512Kb
-  ttl: 7200
+  max: 10 * 1024 * 1024, // Maximum cache size (1mb) defaults to 512Kb
+  ttl: 7200,
 });
 
 let request = requestExt({
-  extensions: [
-    httpRequestCache.extension
-  ]
+  extensions: [httpRequestCache.extension],
 });
 
-let clientID = process.env.GITHUB_CLIENTID || '';
-let clientSecret = process.env.GITHUB_SECRET || '';
+let clientID = process.env.GITHUB_CLIENTID || "";
+let clientSecret = process.env.GITHUB_SECRET || "";
 
-let host = 'https://api.github.com/';
-let auth = new Buffer(`${clientID}:${clientSecret}`).toString('base64')
+let host = "https://api.github.com/";
+const repo = "altany";
+
+let auth = new Buffer(`${clientID}:${clientSecret}`).toString("base64");
 let options = {
   headers: {
-    'User-Agent': 'altany',
-    'Authorization': 'Basic ' + auth
-  }
+    "User-Agent": repo,
+    Authorization: "Basic " + auth,
+  },
 };
 
-let apiVersion = 'v3';
+let apiVersion = "v3";
 
-let marked = require('marked');
-let timeAgo = require('node-time-ago');
+let marked = require("marked");
+let timeAgo = require("node-time-ago");
 
 marked.setOptions({
   renderer: new marked.Renderer(),
@@ -46,91 +46,127 @@ marked.setOptions({
   pedantic: false,
   sanitize: false,
   smartLists: true,
-  smartypants: false
+  smartypants: false,
 });
 
-function formatErrorResponse({ response, message = '', repo, code=500, contentType= 'text/plain'} = {}) {
+function formatErrorResponse({
+  response,
+  message = "",
+  repo,
+  code = 500,
+  contentType = "text/plain",
+} = {}) {
   response.statusCode = code;
-  response.setHeader( 'Content-Type', contentType )
-  return response.end(message + (repo?' for repo "' + repo + '"':''));
+  response.setHeader("Content-Type", contentType);
+  return response.end(message + (repo ? ' for repo "' + repo + '"' : ""));
 }
 
 // App
 const app = express();
-app.use(cors())
+app.use(cors());
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/welcome.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/welcome.html"));
 });
 
-app.get('/repos', function(req, res) {
-  options.url = `${host}users/altany/repos?sort=created`
+app.get("/repos", function (req, res) {
+  options.url = `${host}users/${repo}/repos?sort=created`;
   request(options, function (error, response, body) {
     if (error) {
-      return formatErrorResponse({response: res, message: error});
+      return formatErrorResponse({ response: res, message: error });
+    } else if (response.statusCode !== 200) {
+      return formatErrorResponse({
+        response: res,
+        message: response.body,
+        code: response.statusCode,
+        contentType: "application/javascript",
+      });
     }
-    else if (response.statusCode!==200) {
-      return formatErrorResponse({response: res, message: response.body, code: response.statusCode, contentType: 'application/javascript'});
-    }
-    res.setHeader( 'Content-Type', 'application/json' );
+    res.setHeader("Content-Type", "application/json");
     res.end(body);
   });
 });
 
-app.get('/readme/:repo', function(req, res) {
-  options.url = `${host}repos/altany/${req.params.repo}/contents/README.md`;
-  options.headers['Accept'] = 'application/vnd.github.' + apiVersion + '.raw';
+app.get("/readme/:repo", function (req, res) {
+  options.url = `${host}repos/${repo}/${req.params.repo}/contents/README.md`;
+  options.headers["Accept"] = "application/vnd.github." + apiVersion + ".raw";
   request(options, function (error, response, body) {
     if (error) {
-      return formatErrorResponse({response: res, message: error, repo: req.params.repo});
+      return formatErrorResponse({
+        response: res,
+        message: error,
+        repo: req.params.repo,
+      });
     }
-    if (response.statusCode===404) {
-      return formatErrorResponse({response: res, message: 'README.md not found', repo: req.params.repo, code: 404});
-    }
-    else if (response.statusCode!==200) {
-      return formatErrorResponse({response:res, message: response.body, repo: req.params.repo, code: response.statusCode, contentType: 'text/html'});
-    }
-    else {
-      res.setHeader( 'Content-Type', 'text/html' );
+    if (response.statusCode === 404) {
+      return formatErrorResponse({
+        response: res,
+        message: "README.md not found",
+        repo: req.params.repo,
+        code: 404,
+      });
+    } else if (response.statusCode !== 200) {
+      return formatErrorResponse({
+        response: res,
+        message: response.body,
+        repo: req.params.repo,
+        code: response.statusCode,
+        contentType: "text/html",
+      });
+    } else {
+      res.setHeader("Content-Type", "text/html");
       res.end(marked(body).toString());
     }
   });
 });
 
-app.get('/last-commit/:repo', function(req, res) {
-  options.url = `${host}repos/altany/${req.params.repo}/commits`;
+app.get("/last-commit/:repo", function (req, res) {
+  options.url = `${host}repos/${repo}/${req.params.repo}/commits`;
   request(options, function (error, response, body) {
     if (error) {
-      return formatErrorResponse({response: res, message: error, repo: req.params.repo});
+      return formatErrorResponse({
+        response: res,
+        message: error,
+        repo: req.params.repo,
+      });
     }
     let result = {};
-    if (response.statusCode===404) {
-      return formatErrorResponse({response: res, message: 'Commit history not found', repo: req.params.repo, code: 404});
-    }
-    else if (response.statusCode!==200) {
-      return formatErrorResponse({response: res, message: response.body, repo: req.params.repo, code: response.statusCode, contentType: 'text/html'});
-    }
-    else {
+    if (response.statusCode === 404) {
+      return formatErrorResponse({
+        response: res,
+        message: "Commit history not found",
+        repo: req.params.repo,
+        code: 404,
+      });
+    } else if (response.statusCode !== 200) {
+      return formatErrorResponse({
+        response: res,
+        message: response.body,
+        repo: req.params.repo,
+        code: response.statusCode,
+        contentType: "text/html",
+      });
+    } else {
       let commit = JSON.parse(body)[0];
       result = {
-          link: commit.html_url,
-          date: timeAgo(commit.commit.author.date),
-          message: commit.commit.message
-        }
+        link: commit.html_url,
+        date: timeAgo(commit.commit.author.date),
+        message: commit.commit.message,
+      };
     }
-    res.setHeader( 'Content-Type', 'application/json' );
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
   });
 });
 
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
+app.use(function (req, res, next) {
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.sendStatus(err.status || 500);
 });
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log("Running on http://localhost:" + PORT);
